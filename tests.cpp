8b1131e597df3e30467a3fa8ba85fe8a79e6ad79
Hamza-Rauf-9899 <simplekpk724.com>
2022-03-16 15:24:34 +0500

    test4

--- tests.cpp
+++ tests.cpp
@@ -34,7 +34,7 @@ ASSERT_DEATH({
 
 
 TEST(SquareRootTest, PositiveNos) { 
-    ASSERT_EQ(6, squareRoot(36.0));
+    ASSERT_EQ(61, squareRoot(36.0));
     ASSERT_EQ(18.0, squareRoot(324.0));
     GTEST_SKIP() << "Skipping two tests";
     ASSERT_EQ(25.4, squareRoot(645.16));
