234290f51b1b4281e5181a43b7f1177d37448ff3
Hamza-Rauf-9899 <simplekpk724.com>
2022-03-16 12:41:26 +0500

    neww

---
+++ tests.cpp
@@ -0,0 +1,136 @@
+#include "totest.cpp"
+#include <limits.h>
+#include <iostream>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+
+TEST(Check_seg_fault, mem_accessing) {
+
+// EXPECT_NO_THROW({
+//   int* p;
+//     cout << *p;});
+
+// ASSERT_DEATH({
+//     memset((char *)0x0, 1, 100);
+    
+// }, "")<<"Segmentation fault does not occur";
+
+
+ASSERT_DEATH({
+
+  char *str;
+ 
+   /* Stored in read only part of data segment */
+   str = "GfG";    
+ 
+   /* Problem:  trying to modify read only memory */
+   *(str+1) = 'n';
+    
+}, "")<<"Segmentation fault does not occur";
+
+}
+
+
+// TEST(SquareRootTest, PositiveNos) { 
+//     ASSERT_EQ(6, squareRoot(36.0));
+//     ASSERT_EQ(18.0, squareRoot(324.0));
+//     GTEST_SKIP() << "Skipping two tests";
+//     ASSERT_EQ(25.4, squareRoot(645.16));
+//     ASSERT_EQ(0, squareRoot(0.0));
+// }
+ 
+// TEST(SquareRootTest, NegativeNos) {
+//     ASSERT_EQ(-1.0, squareRoot(-15.0));
+//     ASSERT_EQ(-1.0, squareRoot(-0.2));
+// }
+
+// // Tests factorial of 0.
+// TEST(FactorialTest, HandlesZeroInput) {
+//   EXPECT_EQ(Factorial(0), 1);
+// }
+// // Tests factorial of positive numbers.
+// TEST(FactorialTest, HandlesPositiveInput) {
+//   EXPECT_EQ(Factorial(1), 1);
+//   EXPECT_EQ(Factorial(2), 2);
+//   EXPECT_EQ(Factorial(3), 6);
+//   EXPECT_EQ(Factorial(8), 40320);
+// }
+// // Tests IsPrime()
+// // Tests IsPrime()
+
+// // Tests negative input.
+// TEST(IsPrimeTest, Negative) {
+//   // This test belongs to the IsPrimeTest test case.
+
+//   EXPECT_FALSE(IsPrime(-1));
+//   EXPECT_FALSE(IsPrime(-2));
+//   EXPECT_FALSE(IsPrime(INT_MIN));
+// }
+
+// // Tests some trivial cases.
+// TEST(IsPrimeTest, Trivial) {
+//   EXPECT_FALSE(IsPrime(0));
+//   EXPECT_FALSE(IsPrime(1));
+//   EXPECT_TRUE(IsPrime(2));
+//   EXPECT_TRUE(IsPrime(3));
+// }
+
+// // Tests positive input.
+// TEST(IsPrimeTest, Positive) {
+//   EXPECT_FALSE(IsPrime(4));
+//   EXPECT_TRUE(IsPrime(5));
+//   EXPECT_FALSE(IsPrime(6));
+//   EXPECT_TRUE(IsPrime(23));
+// }
+
+
+// TEST(AdditionTest, test2) { 
+//     ASSERT_EQ(2, addition(1,1));  
+//     ASSERT_EQ(0, addition(0,0));
+
+// }
+
+// TEST(equality_test, test3) { 
+//     ASSERT_EQ(true, equal(1,1));  
+//     ASSERT_EQ(false, equal(0,1));
+
+// }
+
+
+// class BankAccountTest : public testing::Test{
+//         protected:
+//         BankAccount *account;
+//         void SetUp()
+//         {
+//                 account = new BankAccount;
+//                  cout <<"Alive\n";}
+//         void TearDown()
+//         {
+//                 delete account;
+//                 cout <<"Dead\n";
+//         }
+// };
+// TEST_F(BankAccountTest, test3){
+//        account->deposite(200);
+//        EXPECT_EQ(200,account->balance);}
+
+// TEST (BankAccount, test1){
+//        BankAccount instance;
+//        instance.deposite(100);
+//        EXPECT_EQ(100,instance.balance);}
+
+// TEST (BankAccount, test2){
+//        BankAccount instance;
+//        instance.deposite(100);
+//       //  ASSERT_EQ(100,instance.balance);
+//        ASSERT_EQ(100,instance.balance);
+//        instance.deduction(50);
+//        EXPECT_EQ(50,instance.balance);}
+
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ totest.cpp
@@ -0,0 +1,84 @@
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+double squareRoot(const double a) {
+    double b = sqrt(a);
+    if(b != b) { // nan check
+        return -1.0;
+    }else{  
+        return sqrt(a);
+    }
+}
+
+int Factorial(int n)    
+{    
+    if(n<0){    
+        return(-1);} /*Wrong value*/      
+    if(n==0){    
+        return(1);}  /*Terminating condition*/    
+    else    
+        {return(n*Factorial(n-1));}    
+}
+
+bool IsPrime(int n) {
+  // Trivial case 1: small numbers
+  if (n <= 1) return false;
+
+  // Trivial case 2: even numbers
+  if (n % 2 == 0) return n == 2;
+
+  // Now, we have that n is odd and n >= 3.
+
+  // Try to divide n by every odd number i, starting from 3
+  for (int i = 3; ; i += 2) {
+    // We only have to try i up to the square root of n
+    if (i > n/i) break;
+
+    // Now, we have i <= n/i < n.
+    // If n is divisible by i, n is not prime.
+    if (n % i == 0) return false;
+  }
+
+  // n has no integer factor in the range (1, n), and thus is prime.
+  return true;
+}
+
+int addition(int a,int b) {
+    return a+b;   
+}
+
+bool equal(int a,int b){
+    if(a==b){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+// void check_seg_fault(){ 
+//      // allocating memory to p
+//     int* p = (int*) malloc(8*sizeof(int));
+     
+//     *p = 100;
+     
+//     // deallocated the space allocated to p
+//     free(p);
+     
+//     // core dump/segmentation fault
+//     //  as now this statement is illegal
+//     *p = 110;}
+
+
+class BankAccount{
+   public:
+   int balance = 0;
+   void deposite(int amount){
+        balance +=amount;
+   }
+   void deduction (int amount)
+   {
+        balance -=amount;
+   }
+};
